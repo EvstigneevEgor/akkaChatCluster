42b8279823502f576fea43223e5dfcf82942df57
Vyacheslav Borisov <sl.borisov.42@outlook.com>
2021-05-25 21:45:48 +0400

    Little bugfix

--- src/main/java/org/example/Messenge.java
+++ src/main/java/org/example/Messenge.java
@@ -9,7 +9,7 @@ public class Messenge {
         this.PostText = new SimpleStringProperty(postText);
     }
     public Messenge(){
-        this.PostText = new SimpleStringProperty("Введите текст сообщения");
+        this.PostText = new SimpleStringProperty("\u0412\u0432\u0435\u0434\u0438\u0442\u0435\u0020\u0442\u0435\u043a\u0441\u0442\u0020\u0441\u043e\u043e\u0431\u0449\u0435\u043d\u0438\u044f");
     }
 
     public String getPostText() {
